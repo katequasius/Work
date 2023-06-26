fe17ac3cdb416964b8e13808ab3da13979720130
mgl0303 <137797483+mgl0303@users.noreply.github.com>
2023-06-26 16:22:10 +0100

    Update Untitled.py

--- Untitled.py
+++ Untitled.py
@@ -5,14 +5,15 @@ import streamlit as st
 
 
 def home_page():
-    st.write("This is home page")
     st.header("Home Page")
+    st.write("Mode1: Select if you have the name and phone number of the investor")
+    st.write("Mode2: Select if you have the name, phone number, and desired location of the investor")
     
 def mode1():
-    st.write("This is Mode 1")
+    st.header("Mode 1")
     
 def mode2():
-    st.write("This is Mode 2")
+    st.header("Mode 2")
 
 add_selectbox = st.sidebar.selectbox(
     "How would you like to be contacted?",
