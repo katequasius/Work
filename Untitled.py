75e8b78b4ed6c6f6dd6a34eca3145642ae1b9de0
katequasius <101994450+katequasius@users.noreply.github.com>
2023-06-26 15:55:11 +0100

    Update Untitled.py

--- Untitled.py
+++ Untitled.py
@@ -5,7 +5,24 @@ import streamlit as st
 st.write("Hello World")
 add_selectbox = st.sidebar.selectbox(
     "How would you like to be contacted?",
-    ("Home", "Mode 1", "Mode 2")
+    page_names_to_funcs.keys())
 )
 
-streamlit run Home_page.py
+def home_page():
+    st.markdown("Home")
+    st.sidebar.markdown("Home")
+    
+def mode1():
+    st.markdown("Mode 1")
+    st.sidebar.markdown("Mode 1")
+    
+def mode2():
+    st.markdown("Mode 2")
+    st.sidebar.markdown("Mode 2")
+    
+page_names_to_funcs = {
+	"Home": Mode2Page,
+    "Mode 1": Mode2Page,
+    "Mode 2": Mode2Page,
+    }
+
