a6528976a26c25e969edbb06cc99569d0053c565
katequasius <101994450+katequasius@users.noreply.github.com>
2023-06-26 16:44:15 +0100

    Update Untitled.py

--- Untitled.py
+++ Untitled.py
@@ -9,7 +9,7 @@ def home_page():
     st.write("Mode 1: Select if you have the name and phone number of the investor")
     st.write("Mode 2: Select if you have the name, phone number, and desired location of the investor")
     image = Image.open('ProgressLogo.PNG')
-    st.image(image, caption=None, width=None, use_column_width=False)
+    st.image(image, caption=None, width=None, use_column_width=True)
     
 def mode1():
     st.header("Mode 1")
