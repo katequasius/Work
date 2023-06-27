7eb0756eb25b755bc70964bc0633d5f618cdcef6
mgl0303 <137797483+mgl0303@users.noreply.github.com>
2023-06-27 12:22:20 +0100

    Update InvestorMessagingWebsite.py

--- InvestorMessagingWebsite.py
+++ InvestorMessagingWebsite.py
@@ -1,5 +1,6 @@
 
 import streamlit as st
+import webbrowser
 from PIL import Image
 
 #Home page setup
@@ -22,11 +23,11 @@ def mode1():
     number = st.text_input('Investor Phone Number *(No spaces or special characters)*')
     sourcer = checkSpaces(st.text_input('Sourcer Name'))
     text = "https://wa.me/" + number + "?text=Hello%20" + investor + ",%0A%0ANice%20to%20be%20in%20contact,%20I'm%20from%20Progress%20Property,%20we%20find%20HMO%20properties%20for%20company%20let%20on%20behalf%20of%20our%20clients%20helping%20them%20grow%20their%20portfolios.%20%0AWe%20have%20been%20in%20contact%20with%20you%20before%20and%20we're%20interested%20in%20seeing%20what%20new%20business%20we%20can%20do!%20%0A%0AI%20look%20forward%20to%20hearing%20back%20from%20you.%20Thanks.%20%0A%0A" + sourcer + "%0AProgress%20Property%20Services%20Ltd%0Ahttps://progresspropertyservices.co"
-    if st.button('Submit'):
+    if st.button('Click to send WhatsApp Message'):
         if checkNumber(number):
             st.write("Invalid number. Do not include spaces or special characters.")
         else:
-            st.write(text)
+            webbrowser.open_new_tab(text)
 
 #Mode 2 setup
 def mode2():
