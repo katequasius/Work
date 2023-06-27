f80dfa554533380cc2104a366f6d53d91f5ee335
katequasius <101994450+katequasius@users.noreply.github.com>
2023-06-27 12:14:37 +0100

    Update InvestorMessagingWebsite.py

--- InvestorMessagingWebsite.py
+++ InvestorMessagingWebsite.py
@@ -18,9 +18,9 @@ def home_page():
 def mode1():
     st.header("Mode 1")
     st.write("Use if you have the **name** and **phone number** of the investor.")
-    investor = st.text_input('Investor Name')
+    investor = checkSpaces(st.text_input('Investor Name'))
     number = st.text_input('Investor Phone Number *(No spaces or special characters)*')
-    sourcer = st.text_input('Sourcer Name')
+    sourcer = checkSpaces(st.text_input('Sourcer Name'))
     text = "https://wa.me/" + number + "?text=Hello%20" + investor + ",%0A%0ANice%20to%20be%20in%20contact,%20I'm%20from%20Progress%20Property,%20we%20find%20HMO%20properties%20for%20company%20let%20on%20behalf%20of%20our%20clients%20helping%20them%20grow%20their%20portfolios.%20%0AWe%20have%20been%20in%20contact%20with%20you%20before%20and%20we're%20interested%20in%20seeing%20what%20new%20business%20we%20can%20do!%20%0A%0AI%20look%20forward%20to%20hearing%20back%20from%20you.%20Thanks.%20%0A%0A" + sourcer + "%0AProgress%20Property%20Services%20Ltd%0Ahttps://progresspropertyservices.co"
     if st.button('Submit'):
         if checkNumber(number):
@@ -32,10 +32,10 @@ def mode1():
 def mode2():
     st.header("Mode 2")
     st.write("Use if you have the **name**, **phone number**, and **desired location** of the investor.")
-    investor = st.text_input('Investor Name')
+    investor = checkSpaces(st.text_input('Investor Name'))
     number = st.text_input('Investor Phone Number *(No spaces or special characters)*')
-    location = st.text_input('Desired Location')
-    sourcer = st.text_input('Sourcer Name')
+    location = checkSpaces(st.text_input('Desired Location'))
+    sourcer = checkSpaces(st.text_input('Sourcer Name'))
     text = "https://wa.me/" + number + "?text=Hello%20" + investor + ",%0A%0ANice%20to%20be%20in%20contact,%20I'm%20from%20Progress%20Property,%20we%20find%20HMO%20properties%20for%20company%20let%20on%20behalf%20of%20our%20clients%20helping%20them%20grow%20their%20portfolios.%20%0AWe%20have%20been%20in%20contact%20with%20you%20before%20and%20we're%20interested%20in%20seeing%20what%20new%20business%20we%20can%20do!%20From%20our%20previous%20contact,%20we%20see%20that%20you're%20interested%20in%20the%20"+location+"%20area.%20%0A%0AI%20look%20forward%20to%20hearing%20back%20from%20you.%20Thanks.%20%0A%0A" + sourcer + "%0AProgress%20Property%20Services%20Ltd%0Ahttps://progresspropertyservices.co"
     if st.button('Submit'):
         if checkNumber(number):
@@ -55,8 +55,13 @@ def checkNumber(num):
         return False
 
 def checkSpaces(str):
+    new = ""
     for i in str:
-        if 
+        if i == ' ':
+            new = new + '%20'
+        else:
+            new = new + i
+    return new
     
 
 #Drop down box on the left-hand side
