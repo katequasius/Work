bd48d6561bf9ace53665dad5bebde08d02f7f9fc
mgl0303 <137797483+mgl0303@users.noreply.github.com>
2023-06-27 11:53:54 +0100

    Update Untitled.py

--- Untitled.py
+++ Untitled.py
@@ -6,9 +6,11 @@ from PIL import Image
 st.set_page_config(page_title="Investor Reachout")
 def home_page():
     st.title("Home Page")
-    st.header("Use the dropdown on the left-hand side to pick which mode of contact you would like to use. Descriptions of each mode are found below.")
-    st.write("Mode 1: Select if you have the name and phone number of the investor")
-    st.write("Mode 2: Select if you have the name, phone number, and desired location of the investor")
+    st.write("Use the dropdown on the left-hand side to pick which mode of contact you would like to use. Descriptions of each mode are found below.")
+    st.header("Mode 1:")
+    st.write("Select if you have the name and phone number of the investor")
+    st.header("Mode 2:")
+    st.write("Select if you have the name, phone number, and desired location of the investor")
     image = Image.open('ProgressLogo.PNG')
     st.image(image, caption=None, width=2, use_column_width=True)
     
