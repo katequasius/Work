46b9e5e2c65d4f3114e7c34794e635d32736f873
mgl0303 <137797483+mgl0303@users.noreply.github.com>
2023-06-27 12:00:06 +0100

    Update Untitled.py

--- Untitled.py
+++ Untitled.py
@@ -10,7 +10,7 @@ def home_page():
     st.header("Mode 1:")
     st.write("Select if you have the name and phone number of the investor")
     st.header("Mode 2:")
-    st.write("Select if you have the name, phone number, and desired location of the investor.")
+    st.write("Select if you have the name, phone number, and desired location of the investor")
     image = Image.open('ProgressLogo.PNG')
     st.image(image, caption=None, width=2, use_column_width=True)
     
@@ -29,7 +29,7 @@ def mode1():
     
 def mode2():
     st.header("Mode 2")
-    st.write("Use if you have the **name**, **phone number**, and **desired location** of the investor")
+    st.write("Use if you have the **name**, **phone number**, and **desired location** of the investor.")
     investor = st.text_input('Investor Name')
     number = st.text_input('Investor Phone Number *(No spaces or special characters)*')
     location = st.text_input('Desired Location')
