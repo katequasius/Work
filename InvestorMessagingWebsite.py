5cec533e6366dc8c2a9da23fcef5bcb4774670bc
mgl0303 <137797483+mgl0303@users.noreply.github.com>
2023-06-28 12:04:50 +0100

    Update InvestorMessagingWebsite.py

--- InvestorMessagingWebsite.py
+++ InvestorMessagingWebsite.py
@@ -23,6 +23,7 @@ def mode1():
     countryCode = st.text_input('Country Code *(No spaces or special characters)*')
     number = st.text_input('Investor Phone Number *(No spaces or special characters)*')
     sourcer = checkSpaces(st.text_input('Sourcer Name'))
+    #Error Checking
     if st.button('Click to generate WhatsApp Message'):
         if countryCode == "":
             st.write("Error. Need to include a country code.")
@@ -42,9 +43,11 @@ def mode2():
     number = st.text_input('Investor Phone Number *(No spaces or special characters)*')
     location = checkSpaces(st.text_input('Desired Location'))
     sourcer = checkSpaces(st.text_input('Sourcer Name'))
-    text = "https://wa.me/" + number + "?text=Hello%20" + investor + ",%0A%0ANice%20to%20be%20in%20contact,%20I'm%20from%20Progress%20Property,%20we%20find%20HMO%20properties%20for%20company%20let%20on%20behalf%20of%20our%20clients%20helping%20them%20grow%20their%20portfolios.%20%0AWe%20have%20been%20in%20contact%20with%20you%20before%20and%20we're%20interested%20in%20seeing%20what%20new%20business%20we%20can%20do!%20From%20our%20previous%20contact,%20we%20see%20that%20you're%20interested%20in%20the%20"+location+"%20area.%20%0A%0AI%20look%20forward%20to%20hearing%20back%20from%20you.%20Thanks.%20%0A%0A" + sourcer + "%0AProgress%20Property%20Services%20Ltd%0Ahttps://progresspropertyservices.co"
+    #Error Checking
     if st.button('Click to generate WhatsApp Message'):
-        if checkNumber(number):
+        if countryCode == "":
+            st.write("Error. Need to include a country code.")
+        elif checkNumber(number):
             st.write("Invalid number. Do not include spaces or special characters.")
         elif checkNumber(countryCode):
             st.write("Invalid number. Do not include spaces or special characters.")
