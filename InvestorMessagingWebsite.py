ece31fed4e9cac83f70c6561b61c2b76fc952a01
mgl0303 <137797483+mgl0303@users.noreply.github.com>
2023-06-28 14:54:13 +0100

    Update InvestorMessagingWebsite.py

--- InvestorMessagingWebsite.py
+++ InvestorMessagingWebsite.py
@@ -30,17 +30,15 @@ def messageSelect(sourcer, investor, countryCode, number):
     if message == "Message 1":
         st.write("WhatsApp Message [Link](https://wa.me/" + countryCode + number + "?text=Hello%20" + investor + ",%0A%0ANice%20to%20be%20in%20contact,%20I'm%20from%20Progress%20Property,%20we%20find%20HMO%20properties%20for%20company%20let%20on%20behalf%20of%20our%20clients%20helping%20them%20grow%20their%20portfolios.%20%0AWe%20have%20been%20in%20contact%20with%20you%20before%20and%20we're%20interested%20in%20seeing%20what%20new%20business%20we%20can%20do!%20%0A%0AI%20look%20forward%20to%20hearing%20back%20from%20you.%20Thanks.%20%0A%0A" + sourcer + "%0AProgress%20Property%20Services%20Ltd%0Ahttps://progresspropertyservices.co)")
         st.write("**Message 1**  \nHello [Investor Name],  \nNice to be in contact, I'm from Progress Property, we find HMO properties for company let on behalf of our clients helping them grow their portfolios.  \n\nWe have been in contact with you before and we're interested in seeing what new business we can do!  \n\n Look forward to hearing back from you. Thanks.  \n\n[Sourcer Name]  \nProgress Property Services Ltd  \nprogresspropertyservices.co")
-    elif message == "Message 2":
+    if message == "Message 2":
         st.write("WhatsApp Message [Link](https://wa.me/" + countryCode + number + "?text=Hello,%20"+investor+"%0ANice%20to%20be%20in%20contact.%0A%0AI'm%20representing%20Progress%20Property%20Services%20Ltd%20-%20specialists%20in%20finding%20high%20yielding%20company%20let%20opportunities.%0A%0AWe%20came%20across%20your%20company%20and%20we%20are%20interested%20in%20what%20business%20we%20can%20do!%0A%0AI%20look%20forward%20to%20hearing%20from%20you.%20Thanks,%0A%0A"+sourcer+"%20|%20Business%20Development%20Consultant%0AProgress%20Property%20Services%20Ltd%0Ahttps://progresspropertyservices.co")
         st.write("**Message 2**  \nHello, [Investor Name]  \nNice to be in contact.  \n\nI'm representing Progress Property Services Ltd - specialists in finding high yielding company let opportunities.  \n\nWe came across your company and we are interested in what business we can do!  \n\nI look forward to hearing from you. Thanks,  \n\n[Sourcer Name] | Business Development Consultant  \nProgress Property Services Ltd  \nprogresspropertyservices.co")
-    elif message == "Message 3":
+    if message == "Message 3":
         st.write("")
         st.write("**Message 3**  \nHello [Investor Name],  \nI'm from Progress Property. We assist management companies in London. A lot of our clients are companies who are looking to house working professionals, social housing providers, letting agents...etc.  \n\nWe started doing business three years ago and have seen good results and are looking to invest more money into our property finding efforts in order to ensure that there are very few 'fit-for-purpose' properties in the UK that go overlooked.  \n\nCan you give me a similar scope for what you do? Do you deal with other companies similar to ourselves or are you a lettings company yourself?  \n\nWill this be our best contact for us to send you our latest deals?  \n\nThanks,  \n[Sourcer Name]  \nProgress Property Services Ltd  \nprogresspropertyservices.")
                 
     
 def home_page():
-    check = 0
-    
     st.title("Investor Contact Form")
     st.write("Fields marked with a * are required")
     investor = checkSpaces(st.text_input('Investor Name *'))
@@ -52,7 +50,8 @@ def home_page():
     location = checkSpaces(st.text_input('Desired Location'))
     sourcer = checkSpaces(st.text_input('Sourcer Name *'))
     #Error Checking
-    if st.button('Click to generate WhatsApp Message'):
+    if st.button('Click to generate WhatsApp Message') or st.session_state["clicked"]:
+        st.session_state["clicked"] = True
         if countryCode == "":
             st.write("Error. Need to include a country code.")
         elif checkNumber(number):
@@ -63,9 +62,7 @@ def home_page():
             if location != "":
                 st.write("WhatsApp Message [Link](https://wa.me/" + countryCode + number + "?text=Hello%20" + investor + ",%0A%0ANice%20to%20be%20in%20contact,%20I'm%20from%20Progress%20Property,%20we%20find%20HMO%20properties%20for%20company%20let%20on%20behalf%20of%20our%20clients%20helping%20them%20grow%20their%20portfolios.%20%0AWe%20have%20been%20in%20contact%20with%20you%20before%20and%20we're%20interested%20in%20seeing%20what%20new%20business%20we%20can%20do!%20From%20our%20previous%20contact,%20we%20see%20that%20you're%20interested%20in%20the%20"+location+"%20area.%20%0A%0AI%20look%20forward%20to%20hearing%20back%20from%20you.%20Thanks.%20%0A%0A" + sourcer + "%0AProgress%20Property%20Services%20Ltd%0Ahttps://progresspropertyservices.co)")
             else:
-                check = 1
-    if check == 1:
-        messageSelect(sourcer, investor, countryCode, number)
+                messageSelect(sourcer, investor, countryCode, number)
                 
 home_page()
 
