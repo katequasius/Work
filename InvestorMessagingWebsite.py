07e6ad46be1d33c82934baf1de3fc184c25b6a7f
mgl0303 <137797483+mgl0303@users.noreply.github.com>
2023-06-28 11:56:27 +0100

    Update InvestorMessagingWebsite.py

--- InvestorMessagingWebsite.py
+++ InvestorMessagingWebsite.py
@@ -26,6 +26,8 @@ def mode1():
     if st.button('Click to generate WhatsApp Message'):
         if checkNumber(number):
             st.write("Invalid number. Do not include spaces or special characters.")
+        elif checkNumber(countryCode):
+            st.write("Invalid number. Do not include spaces or special characters.")
         else:
             st.write("WhatsApp Message [Link](https://wa.me/" + countryCode + number + "?text=Hello%20" + investor + ",%0A%0ANice%20to%20be%20in%20contact,%20I'm%20from%20Progress%20Property,%20we%20find%20HMO%20properties%20for%20company%20let%20on%20behalf%20of%20our%20clients%20helping%20them%20grow%20their%20portfolios.%20%0AWe%20have%20been%20in%20contact%20with%20you%20before%20and%20we're%20interested%20in%20seeing%20what%20new%20business%20we%20can%20do!%20%0A%0AI%20look%20forward%20to%20hearing%20back%20from%20you.%20Thanks.%20%0A%0A" + sourcer + "%0AProgress%20Property%20Services%20Ltd%0Ahttps://progresspropertyservices.co)")
 
@@ -34,6 +36,7 @@ def mode2():
     st.header("Mode 2")
     st.write("Use if you have the **name**, **phone number**, and **desired location** of the investor.")
     investor = checkSpaces(st.text_input('Investor Name'))
+    countryCode = st.text_input('Country Code *(No spaces or special characters)*')
     number = st.text_input('Investor Phone Number *(No spaces or special characters)*')
     location = checkSpaces(st.text_input('Desired Location'))
     sourcer = checkSpaces(st.text_input('Sourcer Name'))
@@ -41,8 +44,10 @@ def mode2():
     if st.button('Click to generate WhatsApp Message'):
         if checkNumber(number):
             st.write("Invalid number. Do not include spaces or special characters.")
+        elif checkNumber(countryCode):
+            st.write("Invalid number. Do not include spaces or special characters.")
         else:
-            st.write("WhatsApp Message [Link](https://wa.me/" + number + "?text=Hello%20" + investor + ",%0A%0ANice%20to%20be%20in%20contact,%20I'm%20from%20Progress%20Property,%20we%20find%20HMO%20properties%20for%20company%20let%20on%20behalf%20of%20our%20clients%20helping%20them%20grow%20their%20portfolios.%20%0AWe%20have%20been%20in%20contact%20with%20you%20before%20and%20we're%20interested%20in%20seeing%20what%20new%20business%20we%20can%20do!%20From%20our%20previous%20contact,%20we%20see%20that%20you're%20interested%20in%20the%20"+location+"%20area.%20%0A%0AI%20look%20forward%20to%20hearing%20back%20from%20you.%20Thanks.%20%0A%0A" + sourcer + "%0AProgress%20Property%20Services%20Ltd%0Ahttps://progresspropertyservices.co)")
+            st.write("WhatsApp Message [Link](https://wa.me/" + countryCode + number + "?text=Hello%20" + investor + ",%0A%0ANice%20to%20be%20in%20contact,%20I'm%20from%20Progress%20Property,%20we%20find%20HMO%20properties%20for%20company%20let%20on%20behalf%20of%20our%20clients%20helping%20them%20grow%20their%20portfolios.%20%0AWe%20have%20been%20in%20contact%20with%20you%20before%20and%20we're%20interested%20in%20seeing%20what%20new%20business%20we%20can%20do!%20From%20our%20previous%20contact,%20we%20see%20that%20you're%20interested%20in%20the%20"+location+"%20area.%20%0A%0AI%20look%20forward%20to%20hearing%20back%20from%20you.%20Thanks.%20%0A%0A" + sourcer + "%0AProgress%20Property%20Services%20Ltd%0Ahttps://progresspropertyservices.co)")
 
 #Phone number check to make sure there are no spaces or special characters
 def checkNumber(num):
