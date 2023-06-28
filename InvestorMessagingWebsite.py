1f9ee874ac845748f176836d844c338796ac9502
katequasius <101994450+katequasius@users.noreply.github.com>
2023-06-28 12:55:00 +0100

    Update InvestorMessagingWebsite.py

--- InvestorMessagingWebsite.py
+++ InvestorMessagingWebsite.py
@@ -5,6 +5,26 @@ from PIL import Image
 
 #Home page setup
 st.set_page_config(page_title="Investor Reachout")
+def checkNumber(num):
+    check = 0
+    for i in num:
+        if i == "1" or i == "2" or i == "3" or i == "4" or i == "5" or i == "6" or i == "7" or i == "8" or i == "9" or i == "0":
+            check += 1
+    if check != len(num):
+        return True
+    else: 
+        return False
+        
+#Replaces space character with a '%20' to make the text work for the URL
+def checkSpaces(str):
+    new = ""
+    for i in str:
+        if i == ' ':
+            new = new + '%20'
+        else:
+            new = new + i
+    return new
+    
 def home_page():
     st.title("Investor Contact Form")
     st.write("Fields marked with a * are required")
@@ -84,23 +104,4 @@ home_page()
 #             st.write("WhatsApp Message [Link](https://wa.me/" + countryCode + number + "?text=Hello%20" + investor + ",%0A%0ANice%20to%20be%20in%20contact,%20I'm%20from%20Progress%20Property,%20we%20find%20HMO%20properties%20for%20company%20let%20on%20behalf%20of%20our%20clients%20helping%20them%20grow%20their%20portfolios.%20%0AWe%20have%20been%20in%20contact%20with%20you%20before%20and%20we're%20interested%20in%20seeing%20what%20new%20business%20we%20can%20do!%20From%20our%20previous%20contact,%20we%20see%20that%20you're%20interested%20in%20the%20"+location+"%20area.%20%0A%0AI%20look%20forward%20to%20hearing%20back%20from%20you.%20Thanks.%20%0A%0A" + sourcer + "%0AProgress%20Property%20Services%20Ltd%0Ahttps://progresspropertyservices.co)")
 
 #Phone number check to make sure there are no spaces or special characters
-def checkNumber(num):
-    check = 0
-    for i in num:
-        if i == "1" or i == "2" or i == "3" or i == "4" or i == "5" or i == "6" or i == "7" or i == "8" or i == "9" or i == "0":
-            check += 1
-    if check != len(num):
-        return True
-    else: 
-        return False
-        
-#Replaces space character with a '%20' to make the text work for the URL
-def checkSpaces(str):
-    new = ""
-    for i in str:
-        if i == ' ':
-            new = new + '%20'
-        else:
-            new = new + i
-    return new
-    
+
